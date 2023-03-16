f08f62acc524bb6a1d7e9e7b1fa8af80e8084276
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2023-03-16 13:34:27 -0600

    chore: update aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,14 +1,15 @@
 alias c=clear
 alias dcp=docker-compose
 alias cat=bat
-alias nr='npm run'
+alias nr="npm run"
+alias ls="exa -la"
 
 # brew
-alias brewup='brew update && brew upgrade && brew upgrade --cask && brew cleanup'
+alias brewup="brew update && brew upgrade && brew upgrade --cask && brew cleanup"
 
 # git
-alias gunc='git reset --soft HEAD^'
-alias grpo='git remote prune origin'
+alias gunc="git reset --soft HEAD^"
+alias grpo="git remote prune origin"
 
 # prettier
 alias pretty="prettier --write '{,!(dist|public|.cache|target)/**/}*.+(js|jsx|ts|tsx|json|yml|yaml|md|html|css|less|scss|graphql)'"
